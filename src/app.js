8f9cdb73f956a0df117ed5b6bfead6bfe7b29d81
Gustavo Rodrigues <qgustavor@users.noreply.github.com>
2024-07-10 19:13:11 -0300

    It should be lower case
    I guess that the library changed since that comment was published. I tested using developer tools and it works when lower cased.

--- src/app.js
+++ src/app.js
@@ -425,7 +425,7 @@ const handleKeyboardShortcuts = (evt) => {
   if (evt.key === "s" && evt.ctrlKey) {
     evt.preventDefault();
     saveActiveWorkingHandle();
-    editor.exec("Strike");
+    editor.exec("strike");
   }
 };
 
